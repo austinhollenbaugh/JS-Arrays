909409a1e5950eb981672e2e42dbc2a979db942e
Austin <austinhollenbaugh@gmail.com>
2016-07-05 11:16:45 -0600

    completed through both

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,7 +5,9 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
   //Code Here
-
+function first(arr) {
+  return arr[0];
+}
 
 //Next problem
 
@@ -16,7 +18,9 @@ var arr = [40,50,60];
 
 
   //Code Here
-
+function last(arr) {
+  return arr.pop();
+}
 
 //Next Problem
 
@@ -26,6 +30,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
   //Code Here
 
+function looper(family) {
+  for (var i = 0; i < family.length; i++) {
+    alert(family[i]);
+  }
+}
 
 //Next problem
 
@@ -36,6 +45,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
   //Code Here
 
+function reversedLooper(letters) {
+  for (var i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
+
 
 //Next Problem
 
@@ -45,6 +60,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
   //Code Here
 
+function evenFinder(nums) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 != 0) {
+      nums.splice(i, 1);
+    }
+  }
+  return nums;
+}
 
 //Next problem
 
@@ -56,7 +79,21 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 
   //Code Here
-
+function divider(numbersArray) {
+  var result = [];
+  var evensArr = [];
+  var oddsArr = [];
+  for (var i = 0; i < numbersArray.length; i++) {
+    if (numbersArray[i] % 2 === 0) {
+      evensArr.push(numbersArray[i]);
+    } else { 
+      oddsArr.push(numbersArray[i]);
+    }
+  }
+  result.push(evensArr);
+  result.push(oddsArr);
+  return result;
+};
 
 //Next Problem
 
@@ -70,6 +107,17 @@ var getRandomArbitrary = function() {
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
   //Code Here
+function finder(array) {
+  var randNum = getRandomArbitrary();
+  console.log("randNum " + randNum);
+  console.log("array " + array)
+  for (var i = 0; i < array.length; i++) {
+    if (randNum === array[i]) {
+      return true;
+    }
+  }
+  return false;
+};
 
   //Code Here
 
@@ -82,28 +130,52 @@ var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
 
   //Code Here
-
+function reverse(str) {
+  str = str.split('').reverse().join('');
+  return str;
+};
 
 //Next Problem
 
 
 var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 /*
-  Here we're going to write a function that mimics going shopping and checking things off of our grocery list,
-  and adding new items to our list.
+  Here we're going to write a function that mimics going shopping and checking things off of our grocery list, and adding new items to our list.
 
   Write a function called removeItem that is given two arguments, the first is myGroceryList, and the
-  second is an item to remove from myGroceryList. If the second argument (or the item to add or remove) matches an item in myGroceryList,
-  remove that item from the your grocery list and return the new, updated grocery list.
+  second is an item to remove from myGroceryList. If the second argument (or the item to add or remove) matches an item in myGroceryList, remove that item from the your grocery list and return the new, updated grocery list.
 
-  Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList
-  and the second is an item to add to your grocery list. In addItem add the item you passed in to
-  myGroceryList then return the new, updated grocery list.
+  Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList and the second is an item to add to your grocery list. In addItem add the item you passed in to myGroceryList then return the new, updated grocery list.
 
   In both the removeItem function and the addItem function, you will also need to check for valid aurguments. Specrunner will try to call your functions without passing in valid aurguments. When this happens, you will need to respond by returning an empty array.
 */
 
   //Code Here
+
+  function removeItem(myGroceryList, itemToRemove) {
+    var arr = [];
+    if (myGroceryList) {
+        for (var i = 0; i < myGroceryList.length; i++) {
+
+          if (myGroceryList[i] === itemToRemove) {
+            myGroceryList.splice(i, 1);
+          }
+        }
+        return myGroceryList;
+      }
+    return arr;
+  };
+
+  function addItem(myGroceryList, itemToAdd) {
+    var arr = [];
+    if (myGroceryList) {
+      
+      myGroceryList.push(itemToAdd);
+      return myGroceryList;
+    }
+    return arr;
+    
+  };
 
 //removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
@@ -118,6 +190,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
   //Code Here
 
+function maker() {
+  var arr = [];
+  for (var i = 1; i < 216; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
 
 
 //Next Problem
@@ -129,6 +208,14 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 
   //Code Here
 
+function addTen(numbers) {
+  var newArr = [];
+  for (var i = 0; i < numbers.length; i++) {
+    numbers[i] = parseInt(numbers[i]) + 10;
+    newArr.push(numbers[i]);
+  }
+  return newArr;
+};
 
 
 //Next Problem
@@ -150,6 +237,13 @@ for(var i = 0; i < num2; i++){
 
   //Code Here
 
+  function longer (arr1, arr2) {
+    if (arr1.length > arr2.length) {
+      return arr1;
+    }
+    return arr2;
+  }
+
 
 /*As a continuation of the previous problem, write another function called 'both'.
   Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example).
@@ -158,7 +252,22 @@ for(var i = 0; i < num2; i++){
 */
 
   //Code Here
+function both(arr1, arr2) {
+  var newArr = [];
+  for (var i = 0; i < arr1.length; i++) {
+    
+    for (var j = 0; j < arr2.length; j++) {
+      
+      if (arr1[i] === arr2[j]) {
+        newArr.push(arr1[i]);
+      }
 
+    }
+
+  }
+  return newArr;
+
+};
 
 
 
